ed5683382527c785e8df384d2594d68e4a44f1c6
MisterDenFisher <solohad2005@yndex.ru>
2024-04-04 00:48:39 +0300

    Добавьте файлы проекта.

---
+++ Client.cpp
@@ -0,0 +1,85 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+SOCKET Connection;
+
+void ClearCin() {
+	cin.clear();
+	cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+}
+
+void ClientHandler() {
+	int msg_size;
+	while (true) {
+		recv(Connection, (char*)&msg_size, sizeof(int), NULL);
+		char* msg = new char[msg_size + 1];
+		msg[msg_size] = '\0';
+		recv(Connection, msg, msg_size, NULL);
+		cout << msg;
+		delete[] msg;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	/*while (true) {
+		cout << "Подключиться к серверу, чтоб начать игру? [y/n]   ";
+		char startProgram;
+		cin >> startProgram;
+		if (startProgram == 'y') {
+			break;
+		}
+		else {
+			cout << "Вы ввели NO или ошибочное выражение. Не выходить из программы? [y/n]   ";
+			ClearCin();
+			cin >> startProgram;
+			if (startProgram != 'y') {
+				return 0;
+			}
+		}
+	}*/
+
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Ошибка" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+		cout << "Ошибка: не удалось подключиться к серверу.\n";
+		return 1;
+	}
+	cout << "Соединение с сервером установлено!\n";
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+	string msg1;
+	while (true) {
+		getline(cin, msg1);
+		int msg_size = msg1.size();
+		send(Connection, (char*)&msg_size, sizeof(int), NULL);
+		send(Connection, msg1.c_str(), msg_size, NULL);
+		Sleep(10);
+	}
+
+	system("pause");
+	return 0;
+}
